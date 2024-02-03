3ea975dd83c5cf6d61274c4e96dae561cc28e21c
Saifullah Talukder <100260341+saifullah-talukder@users.noreply.github.com>
2024-02-03 16:13:52 +0600

    improved chatbot design

--- next/wheel-of-time/src/components/Heartcare.tsx
+++ next/wheel-of-time/src/components/Heartcare.tsx
@@ -3,7 +3,7 @@ export default function Heartcare() {
     <>
       <iframe
         data-chat-frame="11eec1c0-88f2-81e0-a4ba-131529970b72"
-        className="bg-white border-none w-[550px] h-[700px]"
+        className="bg-white border-none w-[550px] h-[700px] rounded-lg shadow-lg"
       ></iframe>
       <script
         src="http://localhost:3005/api/script/chat.js?iframe&id=11eec1c0-88f2-81e0-a4ba-131529970b72"
